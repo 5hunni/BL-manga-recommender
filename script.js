c8ddd4b4dade539b3c4d4bd259c254e8a3fdd197
5hunni <gacha.source@gmail.com>
2026-09-10 23:27:35 -0500

    Initialize Supabase client and load manga data
    Added Supabase client initialization and manga loading function.

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-console.log("BL Manga Recommender is working!");
+const SUPABASE_URL = "https://iapmfgzltztjxgxpvewt.supabase.co/rest/v1/";
+const SUPABASE_KEY = "sb_publishable_Xl7oe_Oblt0T0z0uR-WwWA_eUguVm0-";
 
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+async function loadManga() {
+  const { data, error } = await supabase
+      .from("manga")
+      .select("*")
+
+  if (error) {
+      console.error("Error loading manga: ", error);
+      return;
+  }
+
+  console.log(data);
+}
+loadManga();
